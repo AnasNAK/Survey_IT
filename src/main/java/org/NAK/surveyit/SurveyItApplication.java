dbe9ee35a105cb55de1c33da0bb0bdc883436313
AnasNAK <anasc00ding@gmail.com>
2024-10-28 13:48:08 +0100

    add dependencies needed

---
+++ src/main/java/org/NAK/surveyit/SurveyItApplication.java
@@ -0,0 +1,11 @@
+package org.NAK.surveyit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyItApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyItApplication.class, args);
+	}
+}
---
+++ src/test/java/org/NAK/surveyit/SurveyItApplicationTests.java
@@ -0,0 +1,13 @@
+package org.NAK.surveyit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
